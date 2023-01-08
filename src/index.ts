5a39ef6e5a0d9eac982b83d55778d40e156e28ee
ARIOS00 <3588315821@qq.com>
2023-01-08 14:24:31 -0500

    patch

--- src/index.ts
+++ src/index.ts
@@ -93,7 +93,7 @@ async function startApolloServer(){
     app.use(express.static(buildPath))
     await app.get("/*", function(req, res){
         res.sendFile(
-            path.join(__dirname, "../news_system_client/build/index.html"),
+            path.join(__dirname, "../../news_system_client/build/index.html"),
             function(err){
                 if(err){
                     res.status(500).send(err)
